d2a825b07428cddbeb2b3ed0ddf2a0c7757711de
pandaemonz <64229401+pandaemonz@users.noreply.github.com>
2020-05-17 02:18:43 +0300

    upload

---
+++ js/ryaba.js
@@ -0,0 +1,33 @@
+const dataURL = "https://api.jsonbin.io/b/5e905926172eb643896166e7";
+
+function handleButton() {
+    $.getJSON(dataURL, handleData);
+}
+
+function handleData(data) {
+    let message = "";
+
+    let obj = {
+      var1: $("input[name=var1]")[0].value,
+      var2: $("input[name=var2]")[0].value,
+      var3: $("input[name=var3]")[0].value,
+      var4: $("input[name=var4]")[0].value,
+      var5: $("input[name=var5]")[0].value,
+      var6: $("input[name=var6]")[0].value,
+      speach: $("input[name=speach]")[0].value
+    }
+    
+    data["text"].forEach(function(item, index) {
+      for (key in obj) {
+        item = item.replace("{" + key + "}", obj[key]);
+      }
+      message = message + item + "<BR>";
+    });
+	$("#result").html(message);
+}
+
+function init() {
+	$("#button-fetch").click(handleButton);
+}
+
+$(document).ready(init);
